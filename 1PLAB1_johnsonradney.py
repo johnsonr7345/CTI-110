51ce3450b22bce0c3456e37072467eddd3a5abe7
johnsonr7345 <johnsonr7345@student.faytechcc.edu>
2025-11-10 20:37:56 -0500

    first python assignment
    taking input from user & printing output.

---
+++ 1PLAB1_johnsonradney.py
@@ -0,0 +1,8 @@
+#Radney Johnson
+#p1lab1.py
+#input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last_name: ")
+
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
