c2b1a4af7e24490e9acc26fcf6f5bd65cf580c3d
johnsonr7345 <johnsonr7345@student.faytechcc.edu>
2025-11-16 03:39:18 -0500

    P1HW2
    this program calculates and displays travel expenses

---
+++ P1HW2.py
@@ -0,0 +1,43 @@
+# Radney Johnson
+# 11/10/2025
+# P1HW2
+# this program calculates and displays travel expenses
+
+
+print("---------Travel expense---------")
+
+
+Budget = input("Enter budget: ")
+Destination = input("Enter the travel destination: ")
+Gas = input("How much do you think you will spend on gas? ")
+Accommodation = input("How much would you need for accommadation ?")
+Food = input("Last, what is the amount you need for food ? ")
+Remaining_balance = input("travel_expenses = gas + accommodation + food")
+
+# calculate addition and subtraction
+
+
+print("-----Travel expenses---------")
+
+print("Location:", Destination)
+print("Initial Budget:", Budget)
+print("Fuel:", Gas)
+print("Accommodation:", Accommodation)
+print("Food:", Food)
+print("Remaining_balance:, remaining balance " )
+
+#calcucate remaining_balance 
+
+# calculate addition and subtraction
+print("---------Addition and Subtraction---------")
+print()
+
+num1 =int(input("Enter a starting integer: "))
+num2 =int(input("Enter a integer to subtract: "))
+num3 =int(input("Enter a integer to subtract: "))
+num4 = int(input("Enter a integer to subtract: "))
+
+final_result = num2 + num3 + num4 - num1
+
+print(num2, "+", num3, "-", num4, "-", num1, "is equal to", final_result, "!!")
+#display results
